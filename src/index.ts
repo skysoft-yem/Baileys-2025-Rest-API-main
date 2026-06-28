08272a1580b0053beaba8799dd4b3ecaadf4fe55
skysoft-yem <amatth050@gmail.com>
2026-06-28 22:21:10 +0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,8 @@
 import makeWASocket from './Socket'
 
 export * from '../WAProto'
-export * from './utils'
-export * from './types'
+export * from './Utils'
+export * from './Types'
 export * from './Defaults'
 export * from './WABinary'
 export * from './WAM'
